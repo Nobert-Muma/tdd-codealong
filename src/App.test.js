433dfb1d84b6865d9a885ab5b6213651b69b78f4
Nobert-Muma <mumanobert17@gmail.com>
2024-08-17 20:37:02 +0300

    Used testing tools included with Create React App.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,18 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // Arrange
   render(<App />);
+
+  // Using debug method to give us a sense of what the DOM looks like when our tests are running
+  screen.debug();
+
+  // Act
   const linkElement = screen.getByText(/learn react/i);
+
+  // To debug a single element:
+  screen.debug(linkElement);
+
+  // Assert
   expect(linkElement).toBeInTheDocument();
 });
